cfff1eed74b2a684008932133f8daac05426d97f
Renseva <94011506+Renseva@users.noreply.github.com>
2022-09-08 20:55:18 +0300

    Add files via upload

---
+++ day1task.py
@@ -0,0 +1,18 @@
+# I am very uncomfortable with sharing my homework with the world but I guess that's what I am required to do, I don't know why :(
+
+#Write the following code in Python:
+#print your name 5 times
+print("Renseva"*5)
+
+#print Bananananana using "Ba" once and "na" 5 times
+print("Ba"+"na"*5)
+
+#print the number of seconds in a year calculated from months, days, minutes, seconds
+print((7*31*24*60*60)+28+(4*30*24*60*60))
+
+#how much a googol is that is 10 to the power of 100th
+print(10**100)
+
+#how much will your 1000 Euro investment be worth after 12 years of 6% yearly interest?
+# I use this formula "future value = present value x (1+ interest rate)n"
+print(1000*((1+0.06)**12))
