f6c654f157812bf0e822776dc7cda5e999068bcd
Renseva <94011506+Renseva@users.noreply.github.com>
2022-09-23 18:16:22 +0300

    reviewed

--- day7task2.py
+++ day7task2.py
@@ -7,17 +7,26 @@
 # is_palindrome("nava") -> False
 
 def is_palindrome(text):
-    # text_list = text.split("")
-    n = 0
-    m = 0
-    for i in range(0, len(text)):
-        if text[0 + n] == text [-1 + m]:
-            result = True
-            n += 1
-            m -= 1
-        else:
-            result = False
-            break
-    return result
-word = input("Please enter a word: ")
-print(is_palindrome(word))
+    phrase = text.lower().replace(" ", "*")
+    return phrase == phrase[::-1]
+
+some_text = input("Please enter some text: ")
+print(is_palindrome(some_text))
+
+# initially I used .split method to make a list from sentence however not checked for one word which in case of one word makes a list
+# with that word as the single list element
+
+# below worked for word, was told it's more lower level solution, makes sense given the above.....
+#  def is_palindrome(text):
+#     # text_list = text.split("")
+#     n = 0
+#     m = 0
+#     for i in range(0, len(text)):
+#         if text[0 + n] == text [-1 + m]:
+#             result = True
+#             n += 1
+#             m -= 1
+#         else:
+#             result = False
+#             break
+#     return result
