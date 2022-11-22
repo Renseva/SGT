fc5cff50fb85d3c0418a69fea7d9f145c79c5153
Renseva <renseva.lt@gmail.com>
2022-11-22 21:05:22 +0200

    Add files via upload

---
+++ day32regex.py
@@ -0,0 +1,34 @@
+import re
+
+phone_text = """
++1 555 123 4567
++1 555 123 456
++1 555 123 45
+Latvia +371 20 123 456
+
+Lithuania +370 5 123 456
+
+Estonia +372 5123 456
+
+USA +1 408 123 4567
+
+sometimes we write our phone numbers like this: 555 123 4560
+
+sometimes we write our phone numbers like this: 555-123-4561
+
+sometimes we write our phone numbers like this: 555.123.4562 rarely
+also we use parentheses: (555) 123-4563
+
+so the assignment is to write a regular expression that will match all of these phone numbers
+"""
+# match_phones = re.findall(r"[+]+[0-9]+[(]+[0-9]+[-.)]+[0-9]+[-.]+[0-9]", phone_text)
+# match_phones = re.findall(r"[+]+\d+[(]+\d+[-.)]+\d+[-.]+\d+", phone_text)
+# match_phones = re.findall(r".\d+\d+.\d+\d+.\d+\d+.\d+\d+|.\d+\d+.\d+\d+.\d+\d+|.\d+\d+.\d.\d+\d+.\d+\d+|1.\d+\d+.\d+\d+.\d+\d+|[\(]d+\d+[\)]\d+\d+.\d+\d+", phone_text)
+match_phones = re.findall(r".\d+\d+.\d+\d+.\d+\d+.\d+\d+|.\d+\d+.\d+\d+.\d+\d+|.\d+\d+.\d.\d+\d+.\d+\d+|1.\d+\d+.\d+\d+.\d+\d+|.d+\d+.\d+\d+.\d+\d+", phone_text)
+
+print(match_phones)
+
+# TODO get all of these phone numbers using a regular expression
+# and return them as a list
+# see how many you can get
+# of course you can google for some regex ideas but first see how far you can get on your own
